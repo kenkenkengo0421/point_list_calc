f9127737944f0d80b23b629b381a4dc6ec55ce08
kenkenkengo0421 <kyongtian181@gmail.com>
2026-04-24 00:16:46 +0900

    save______

---
+++ main.js
@@ -0,0 +1,72 @@
+"use strict";
+//入力変数
+let in_point_yen = 0;
+let in_Cpu = 0;
+let in_Unitpoints = 16;
+let in_Boxname = "";
+let in_DOII = 0;
+let in_DOIS = 0;
+let in_DII = 0;
+let in_DIS = 0;
+let in_DIA = 0;
+let in_DO = 0;
+let in_SI = 0;
+let in_AI = 0;
+let in_RI = 0;
+let in_AO = 0;
+//出力変数１
+let out_DOCX = 0;
+let out_DOTX = 0;
+let out_DIS = 0;
+let out_DIA = 0;
+let out_Total = 0;
+//出力変数2
+let out_kv8000 = 0;
+let out_kv_b16x = 0;
+let out_kv_b16t = 0;
+let out_kv_c32x = 0;
+let out_kv_c32t = 0;
+let out_kv_c64x = 0;
+let out_kv_c64t = 0;
+let out_kv_tp40 = 0;
+let out_kv_da40v = 0;
+out_DOCX = in_DOII + in_DOIS + in_DO;
+out_DOTX = in_DOII + in_DOIS + in_DO;
+out_DIS = in_DOII + in_DOIS + in_DII + in_DIS;
+out_DIA = in_DOII + in_DII + in_DIA;
+//in_SI
+//in_RI
+//in_AI
+//in_AO
+//in_Comment
+out_Total = out_DOCX + out_DOTX + out_DIS + out_DIA + in_SI + in_RI + in_AI + in_AO;
+out_kv8000 = in_Cpu;
+//di
+out_kv_b16x = Math.ceil((out_DIS + out_DIA + in_SI) / 16);
+out_kv_c32x = Math.ceil((out_DIS + out_DIA + in_SI) / 32);
+out_kv_c64x = Math.ceil((out_DIS + out_DIA + in_SI) / 64);
+//do
+out_kv_b16t = Math.ceil((out_DOCX + out_DOTX) / 16);
+out_kv_c32t = Math.ceil((out_DOCX + out_DOTX) / 32);
+out_kv_c64t = Math.ceil((out_DOCX + out_DOTX) / 64);
+//ai
+out_kv_tp40 = Math.ceil((in_AI + in_RI) / 4);
+//ao
+out_kv_da40v = Math.ceil(in_AO / 4);
+let Point_list = "";
+if (in_Unitpoints == 16) {
+    Point_list =
+        `
+盤名称:${in_Boxname}
+DO(cx):${out_DOCX}
+DO(tx):${out_DOTX}
+DIS:${out_DIS}
+DIA:${out_DIA}
+SI:${in_SI}
+AI:${in_AI}
+RI:${in_RI}
+AO:${in_AO}
+
+`;
+}
+console.log(Point_list);
---
+++ main.ts
@@ -0,0 +1,86 @@
+//入力変数
+let in_point_yen :number = 0;
+let in_Cpu :number = 0;
+let in_Unitpoints :number = 16;
+let in_Boxname :string = "";
+let in_DOII :number = 0;
+let in_DOIS :number = 0;
+let in_DII :number = 0;
+let in_DIS :number = 0;
+let in_DIA :number = 0;
+let in_DO :number = 0;
+let in_SI :number = 0;
+let in_AI :number = 0;
+let in_RI :number = 0;
+let in_AO :number = 0;
+
+//出力変数１
+let out_DOCX :number = 0;
+let out_DOTX :number = 0;
+let out_DIS :number = 0;
+let out_DIA :number = 0;
+let out_Total :number = 0;
+
+//出力変数2
+let out_kv8000 :number = 0;
+let out_kv_b16x :number = 0;
+let out_kv_b16t :number = 0;
+let out_kv_c32x :number = 0;
+let out_kv_c32t :number = 0;
+let out_kv_c64x :number = 0;
+let out_kv_c64t :number = 0;
+let out_kv_tp40 :number = 0;
+let out_kv_da40v :number = 0;
+
+out_DOCX = in_DOII + in_DOIS + in_DO;
+out_DOTX = in_DOII + in_DOIS + in_DO;
+out_DIS = in_DOII + in_DOIS + in_DII + in_DIS;
+out_DIA = in_DOII + in_DII + in_DIA;
+//in_SI
+//in_RI
+//in_AI
+//in_AO
+//in_Comment
+
+out_Total = out_DOCX + out_DOTX + out_DIS + out_DIA + in_SI + in_RI + in_AI + in_AO;
+out_kv8000 = in_Cpu;
+//di
+out_kv_b16x = Math.ceil((out_DIS + out_DIA + in_SI) / 16);
+out_kv_c32x = Math.ceil((out_DIS + out_DIA + in_SI) / 32);
+out_kv_c64x = Math.ceil((out_DIS + out_DIA + in_SI) / 64);
+//do
+out_kv_b16t = Math.ceil((out_DOCX + out_DOTX) / 16);
+out_kv_c32t = Math.ceil((out_DOCX + out_DOTX) / 32);
+out_kv_c64t = Math.ceil((out_DOCX + out_DOTX) / 64);
+//ai
+out_kv_tp40 = Math.ceil((in_AI + in_RI) / 4);
+//ao
+out_kv_da40v = Math.ceil(in_AO / 4);
+
+let Point_list = "";
+
+if (in_Unitpoints == 16) {
+  Point_list = 
+`
+盤名称:${in_Boxname}
+DO(cx):${out_DOCX}
+DO(tx):${out_DOTX}
+DIS:${out_DIS}
+DIA:${out_DIA}
+SI:${in_SI}
+AI:${in_AI}
+RI:${in_RI}
+AO:${in_AO}
+
+`;
+  
+}
+
+console.log(Point_list);
+
+
+
+
+
+
+
